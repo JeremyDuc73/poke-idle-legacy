d513fc4a8fe0681e15db505a0e1d9d47a372101e
Jeremy Duc <ducjeremy.web@gmail.com>
2026-03-27 01:08:31 +0100

    fix: use named import for BaseSchema in migration (fixes CI build)

--- server/database/migrations/1772890000000_create_add_shiny_charms_to_users_table.ts
+++ server/database/migrations/1772890000000_create_add_shiny_charms_to_users_table.ts
@@ -1,15 +1,17 @@
-import BaseSchema from '@adonisjs/lucid/schema'
+import { BaseSchema } from '@adonisjs/lucid/schema'
 
 export default class extends BaseSchema {
+  protected tableName = 'users'
+
   async up() {
-    this.schema.alterTable('users', (table) => {
-      table.integer('shiny_charms').defaultTo(0)
-      table.jsonb('completed_pokedex_gens').defaultTo('[]')
+    this.schema.alterTable(this.tableName, (table) => {
+      table.integer('shiny_charms').defaultTo(0).notNullable()
+      table.jsonb('completed_pokedex_gens').defaultTo('[]').notNullable()
     })
   }
 
   async down() {
-    this.schema.alterTable('users', (table) => {
+    this.schema.alterTable(this.tableName, (table) => {
       table.dropColumn('shiny_charms')
       table.dropColumn('completed_pokedex_gens')
     })
